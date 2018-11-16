ea0dff89e6d4bbc66a0f756422e87994805b4ad2
Fedor Alekseev <feodor.alexeev@gmail.com>
2018-11-16 14:48:17 +0100

    Minor touches

--- ctz.cc
+++ ctz.cc
@@ -1,6 +1,6 @@
 int main(int argc, const char* argv[]) {
-    static_assert(32 == __builtin_ctz(0));
-    static_assert(0 == __builtin_ctz(0b1111));
-    static_assert(2 == __builtin_ctz(0b10100));
-    return __builtin_ctz(argc);
+  static_assert(32 == __builtin_ctz(0));
+  static_assert(0 == __builtin_ctz(0b1111));
+  static_assert(2 == __builtin_ctz(0b10100));
+  return __builtin_ctz(argc);
 }
--- power_modulo.cc
+++ power_modulo.cc
@@ -4,10 +4,11 @@ constexpr int64_t Modulo = 1000000007;  // a prime number
 auto multiply_modulo = [](int64_t a, int64_t b) {
   return a * b % Modulo;
 };
+// this is required to fully define the operation
+// will be called through ADL
 int64_t identity_element(decltype(multiply_modulo)) {
   return 1;
 }
-
-bool fermat_little_theorem_holds(int64_t x) {  // $x^{p} = x\mod{p}$
+bool fermat_little_theorem_holds(int64_t x) {  // $x^{p} \equiv x\pmod{p}$
   return __gnu_cxx::power(x, Modulo, multiply_modulo) == x % Modulo;
 }
